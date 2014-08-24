fafc67361a47c704a72f17522f8224d1724ce434
Chris Loew <cloew123@gmail.com>
2014-08-24 11:27:35 -0600

    Can now specify a return code from the JSON Controller

--- src/kao_flask/controllers/json_controller.py
+++ src/kao_flask/controllers/json_controller.py
@@ -5,7 +5,14 @@ class JSONController:
     
     def perform(self, *args, **kwargs):
         """ Perform the JSON action """
-        return jsonify(self.performWithJSON(*args, json=request.json, **kwargs))
+        response = self.performWithJSON(*args, json=request.json, **kwargs)
+        
+        code = 200
+        if len(response) == 2:
+            data, code = response
+        else:
+            data = response
+        return jsonify(data), code
         
     def performWithJSON(self, json=None):
         """ Perform the JSON action """
