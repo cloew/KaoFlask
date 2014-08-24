919731c6f09067a9f89ea1c920460f3b9ad1ed1d
Chris Loew <cloew123@gmail.com>
2014-08-23 19:47:48 -0600

    Added install file

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kao_flask',
+      version='0.1',
+      description='Python Flask Wrapper',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['kao_flask'],
+     )
