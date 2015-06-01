1e2583325f2e1484fcb16498244f752f898581f1
cloew <cloew123@gmail.com>
2015-05-31 19:37:52 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_flask',
-      version='0.1',
+      version='0.1.01',
       description='Python Flask Wrapper',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
