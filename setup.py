eeb66ace304d8eebca1ad06e9ec661727086d308
Chris Loew <cloew123@gmail.com>
2015-06-02 13:47:00 -0600

    Added SSLify extension

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name='kao_flask',
-      version='0.1.01',
+      version='0.1.02',
       description='Python Flask Wrapper',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['kao_flask', 'kao_flask.controllers', 'kao_flask.ext', 
-                'kao_flask.ext.sqlalchemy', 'kao_flask.ext.stormpath'],
+                'kao_flask.ext.sqlalchemy', 'kao_flask.ext.sslify', 
+                'kao_flask.ext.stormpath'],
      )
