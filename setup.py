62dac5d6753077a41ab4ffbfb478bc94d22365d0
Chris Loew <cloew123@gmail.com>
2015-06-03 09:40:05 -0600

    Moved Crud logic from the Vocab Tester to the SQLAlchemy Extension

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(name='kao_flask',
-      version='0.1.02',
+      version='0.2.00',
       description='Python Flask Wrapper',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['kao_flask', 'kao_flask.controllers', 'kao_flask.ext', 
-                'kao_flask.ext.sqlalchemy', 'kao_flask.ext.sslify', 
+                'kao_flask.ext.sqlalchemy', 'kao_flask.ext.sqlalchemy.crud',
+                'kao_flask.ext.sslify', 
                 'kao_flask.ext.stormpath'],
      )
