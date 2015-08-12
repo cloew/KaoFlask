affc426f2d3185423492f29fcc675d0f9790f3ec
Chris Loew <cloew123@gmail.com>
2015-08-12 11:12:19 -0600

    Removed the Stormpath Extension

--- kao_flask/ext/stormpath/stormpath_extension.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.stormpath import StormpathManager
-
-class StormpathExtension:
-    """ Represents an extension to add Stormpath characteristics to the Flask server """
-    
-    def initialize(self, server):
-        """ Initialize the Server with the extension """
-        server.stormpathManager = StormpathManager(server.app)
