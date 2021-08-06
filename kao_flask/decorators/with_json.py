ff65ea0a358520f4406aff32f14c141d8f2a8559
Chris Loew <cloew123@gmail.com>
2021-08-06 12:01:39 -0600

    The with_json decorator now uses `functool.wraps` to preserve metadata from the original functiong

--- kao_flask/decorators/with_json.py
+++ kao_flask/decorators/with_json.py
@@ -1,7 +1,9 @@
 from flask import jsonify, request
+from functools import wraps
 
 def with_json(fn):
     """ Decorator to extract the request's Json and pass it to the function as a `json` parameter, then jsonify the result from the fn """
+    @wraps(fn)
     def fn_with_json(*args, **kwargs):
         json = request.get_json()
         response = fn(*args, json=json, **kwargs)
