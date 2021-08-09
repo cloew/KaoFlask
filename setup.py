99d4255eb238af85f9203ffca6a78cac135012aa
Chris Loew <cloew123@gmail.com>
2021-08-09 11:06:44 -0600

    Fixed issue with setup.py file not being updated to include the new decorators subpackage

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='kao_flask',
-      version='0.5.2',
+      version='0.5.3',
       description='Python Flask Wrapper',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['kao_flask',
-                'kao_flask.controllers',
+                'kao_flask.decorators',
                 'kao_flask.ext'],
      )
